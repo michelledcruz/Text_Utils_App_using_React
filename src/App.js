d00cf6006fcb485f16810f95863b6b1c4550c6f7
Michelle Dcruz <52381792+michelledcruz@users.noreply.github.com>
2023-06-16 14:18:54 +1000

    added fragment in app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 function App() {
   return (
+    <>
+    <h1>This is Title</h1>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -15,10 +17,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React mich
         </a>
       </header>
     </div>
+    </>
   );
 }
 
