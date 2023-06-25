012c64d8e4d198c09d6573a0f64816aaf6661358
Michelle Dcruz <52381792+michelledcruz@users.noreply.github.com>
2023-06-25 12:35:40 +1000

    Update About.js

--- src/About.js
+++ src/About.js
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
 
-
 export default function About() {
 
-    const buttonStyle ={ backgroundColor: 'orange', color: 'black'}
+    const buttonStyle ={ backgroundColor: 'black', color: '#af1df3', border: '2px solid rgb(175, 29, 243)', borderRadius: '22px' , padding: '10px 15px', marginTop: '2px'}
 
     const [myStyle, setMyStyle] = useState({
         backgroundColor:"black",
         color:"white",
+        border: "2px solid rgb(175, 29, 243)",
+        borderRadius: '20px',
+
     })
+
+    const myHeadingStyle = { width: '100px'}
 
     const [inputList, setInputList] = useState([]);
 
@@ -21,7 +25,7 @@ export default function About() {
           <div className="accordion-item">
               <h2 className="accordion-header" id={`heading${accId}`}>
               <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target={`#collapse${accId}`} aria-expanded="true" aria-controls={`collapse${accId}`}  style={myStyle}>
-                 To Do Task:{accId}
+                <p className='task-heading' style={myHeadingStyle}> To Do Task:</p><textarea placeholder="Task title..."></textarea>
               </button>
               </h2>
               <div id={`collapse${accId}`} className="accordion-collapse collapse show" aria-labelledby={`heading${accId}`} data-bs-parent="#accordionExample" style={myStyle}>
@@ -64,7 +68,7 @@ export default function About() {
         <div className="accordion-item">
             <h2 className="accordion-header" id={`heading${accId}`}>
             <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target={`#collapse${accId}`} aria-expanded="true" aria-controls={`collapse${accId}`}  style={myStyle}>
-                To Do Task: {accId}
+            <p className='task-heading' style={myHeadingStyle}> To Do Task:</p> <textarea placeholder="Task title..."></textarea>
             </button>
             </h2>
             <div id={`collapse${accId}`} className="accordion-collapse collapse show" aria-labelledby={`heading${accId}`} data-bs-parent="#accordionExample" style={myStyle}>
