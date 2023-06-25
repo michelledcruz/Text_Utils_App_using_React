bceba4bfa59a000a9bb9bbe013df680c8f21b84d
Michelle Dcruz <52381792+michelledcruz@users.noreply.github.com>
2023-06-25 12:31:17 +1000

    Updated the custom task header and improved the UI

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@
 import About from './About';
 import './App.css';
 import Navbar from './Navbar';
-import TextArea from './TextArea';
+// import TextArea from './TextArea';
 
 function App() {
   return (
   <>
-  <Navbar title="TextUtils" text="Dropdown12"/>
+  <Navbar title="To-Do List" text="Dropdown12"/>
   <div className="container">
-  {/* <TextArea heading="Enter your text below"/> */}
+  {/* <TextArea heading="Enter your text below"/>  */}
   <About></About>
   </div>
   </>
